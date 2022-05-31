239900432e839b9e6158a84bbea106110efb303a
adibayu luthfiansyah <adibayuluthfiansyah@gmail.com>
2022-06-01 03:04:54 +0700

    menambahkan paragraph

---
+++ index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>coba git lagi</title>
+</head>
+<body>
+    <h1>hello world!</h1>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ullam debitis deleniti necessitatibus neque perferendis ad dolore. Ullam minima accusantium commodi exercitationem deleniti tenetur vel debitis? Iste iusto dolores dolorem ducimus.</p>
+</body>
+</html>
